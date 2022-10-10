69d7b63fb357458b851c3ecb520fcdb4d8711735
Huaijie Wang <hjwang998244353@gmail.com>
2022-10-10 09:08:09 +0000

    feat: 把游戏文字改回英语

--- game/frontend/game_play.js
+++ game/frontend/game_play.js
@@ -125,7 +125,7 @@ var make_spectator_intf = function() {
       send_cmd(window.tick + ' C');
     }
     window.fow = !window.fow;
-    document.querySelector('#fow').innerHTML = `战争迷雾: ${window.fow ? '开' : '关'}`
+    document.querySelector('#fow').innerHTML = `Fog of War: ${window.fow ? 'ON' : 'OFF'}`
     sendEmptyInst();
   };
 
@@ -153,8 +153,8 @@ var make_spectator_intf = function() {
   window.cmd_input.style.resize = "none";
   window.cmd_input.disabled = "true";
 
-  // document.getElementById("cmd_input_label").innerHTML = "Current order to execute on:";
-  document.getElementById("cmd_input_label").innerHTML = "目前执行的指令:";
+  document.getElementById("cmd_input_label").innerHTML = "Current command to execute on:";
+  // document.getElementById("cmd_input_label").innerHTML = "目前执行的指令:";
   window.cmd_input.style.color = "red";
 
   window.cmd_history = document.getElementById("cmd_history");
@@ -372,8 +372,8 @@ var make_player_intf = function() {
 
   document.getElementById("cmd_hint").style.display = "none";
 
-  // document.getElementById("cmd_input_label").innerHTML = "Current order to execute on:";
-  document.getElementById("cmd_input_label").innerHTML = "目前执行的指令:";
+  document.getElementById("cmd_input_label").innerHTML = "Current command to execute on:";
+  // document.getElementById("cmd_input_label").innerHTML = "目前执行的指令:";
   window.cmd_input.style.color = "red";
 
   window.cmd_history = document.getElementById("cmd_history");
